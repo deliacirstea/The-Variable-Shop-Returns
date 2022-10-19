eebe264848ae8bc07cb838a842f6b6356a141725
deliacirstea <deliutacirstea@gmail.com>
2022-10-19 18:54:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿
+string y = "7th";
+Console.WriteLine("Welcome to my " + y + " challenge!\n");
+Console.WriteLine("--------------------------");
+Console.WriteLine("Example of all the variables disscused in the book - updated version: \n");
+
+
+int a = 15;
+//Console.WriteLine(a);
+
+uint b = 32;
+//Console.WriteLine(b);
+
+short c = 8;
+//Console.WriteLine(c);
+
+ushort d = 9;
+//Console.WriteLine(d);
+
+long e = 3362;
+//Console.WriteLine(e);
+
+ulong f = 32548;
+//Console.WriteLine(f);
+
+byte g = 85;
+//Console.WriteLine(g);
+
+sbyte h = 36;
+//Console.WriteLine(h);
+
+char i = 'A';
+//Console.WriteLine(i);
+
+string j = "sunny days";
+//Console.WriteLine(j);
+
+bool k = true;
+//Console.WriteLine(k);
+
+float l = 4.0f;
+//Console.WriteLine(l);
+
+double m = 5.6;
+//Console.WriteLine(m);
+
+decimal n = 30;
+//Console.WriteLine(n);
+
+
+//new objective - uppdate
+a = 5;
+b = 15;
+c = 10;
+d = 11;
+e = 3;
+f = 65;
+g = 6;
+h = 7;
+i = '%';
+j = "lovely";
+k = false;
+l = 6.0f;
+m = 8.9;
+n = 40;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
+Console.WriteLine(m);
+Console.WriteLine(n);
+
